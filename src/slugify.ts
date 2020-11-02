2f17e56de7f83d88aea68f2281abcfa2316e4eb0
Luke Kozak <kozakluke@gmail.com>
2020-11-02 14:05:05 +0100

    Update slugify util

--- src/slugify.ts
+++ src/slugify.ts
@@ -1,6 +1,26 @@
+const sets = {
+    'ą': 'a', 'ä': 'a',
+    'ć': 'c',
+    'ę': 'e',
+    'ł': 'l',
+    'ń': 'n',
+    'ó': 'o', 'ö': 'o',
+    'ś': 's',
+    'ß': 'ss',
+    'ü': 'u',
+    'ź': 'z', 'ż': 'z',
+    '·': '-', '/': '-', '_': '-', ',': '-', ':': '-', ';': '-'
+};
+
 export default function slugify(str:string) {
     return str
         .toLowerCase()
-        .replace(/[^\w ]+/g,'')
-        .replace(/ +/g,'-')
+        .replace(/\s+/g, '-')       // replace spaces with -
+        .replace(/./g, (char)=>
+            sets[char] || target)   // replace special characters using sets
+        .replace(/&/g, '-and-')     // replace & with 'and'
+        .replace(/[^\w\-]+/g, '')   // remove all non-word chars
+        .replace(/\-\-+/g, '-')     // replace multiple - with single -
+        .replace(/^-+/, '')         // trim - from start of text
+        .replace(/-+$/, '');        // trim - from end of text
 }
